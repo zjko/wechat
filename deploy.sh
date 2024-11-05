33b87679575283f9ee6d6ead5dd105fc7c357803
zjko <zjko@outlook.com>
2024-11-06 01:05:57 +0800

    release commit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 git add .
 git commit -m "release commit"
 git push origin develop
-# npm run build
-# cd dist
-# git init 
-# git remote add origin 
+npm run build
+cd dist
+git init 
+git remote add origin git@github.com:zjko/LLM-Playground.git
+git add .
+git commit -m "release"
+git push origin master
